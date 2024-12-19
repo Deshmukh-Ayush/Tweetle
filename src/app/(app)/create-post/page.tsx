6b7e3dfc4d5beae018b0611b733f704f4cb7359c
Ayush Deshmukh <ayushdeshmukh301@gmail.com>
2024-12-20 03:28:36 +0530

    basic frontend

--- src/app/(app)/create-post/page.tsx
+++ src/app/(app)/create-post/page.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+"use client";
 
-const page = () => {
-  return <div>Create your Post</div>;
-};
+import axios from "axios";
+import { useState } from "react";
 
-export default page;
+export default function CreatePost() {
+  const [content, setContent] = useState("");
+
+  const handleSubmit = async () => {
+    const res = await axios.post("/api/create-post", {
+      method: "POST",
+      headers: { "Content-Type": "application/jsonj" },
+      body: JSON.stringify({ content }),
+      status: 201,
+    });
+
+    if (res.status === 201) {
+      alert("Post created successfully");
+      setContent("");
+    } else {
+      alert("Failed to create post");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Create Post</h1>
+      <input
+        type="text"
+        placeholder="Write your content here"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
 "use client";
 
+interface Blog {
+  id: string;
+  content: string;
+  author: { email: string };
+}
+
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [blogs, setBlogs] = useState<Blog[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/blogs")
+      .then((res) => res.data)
+      .then((data) => setBlogs(data));
+  }, []);
+
   return (
     <div>
       <SignedOut>
@@ -12,13 +29,13 @@ export default function Home() {
         <UserButton />
       </SignedIn>
       <h1 className="text-2xl">Tweetle</h1>
-      <h3 className="text-xl">Folder Structure</h3>
-      <p>Home Page</p>
-      <p>Sign-up Page</p>
-      <p>Sign-in Page</p>
-      <p>Create Post</p>
-      <p>Show Your Post</p>
-      <p>Contact</p>
+      <h1>All Blogs</h1>
+      {blogs.map((blog) => (
+        <div key={blog.id}>
+          <h2>By: {blog.author.email}</h2>
+          <p>Content: {blog.content}</p>
+        </div>
+      ))}
     </div>
   );
 }
