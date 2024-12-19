8f70024ca27f82a0f9ce139601c8791d2d05e4b5
Ayush Deshmukh <ayushdeshmukh301@gmail.com>
2024-12-19 16:14:40 +0530

    neon db configured and created models

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,15 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div>
       <SignedOut>
         <SignInButton />
       </SignedOut>
       <SignedIn>
         <UserButton />
       </SignedIn>
-      <h1 className="text-5xl">Tweetle</h1>
-      <h3>Folder Structure</h3>
+      <h1 className="text-2xl">Tweetle</h1>
+      <h3 className="text-xl">Folder Structure</h3>
       <p>Home Page</p>
       <p>Sign-up Page</p>
       <p>Sign-in Page</p>
